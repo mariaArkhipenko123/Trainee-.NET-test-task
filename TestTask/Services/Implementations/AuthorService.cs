1989fa0dbf07e09de3b76475638dde649ab44e94
Мария Архипенко <maria.arhipenko@interexy.com>
2024-12-18 02:32:49 +0300

    Added services with project logic

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,40 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorService(IBookRepository bookRepository, IAuthorRepository authorRepository)
+        {
+            _bookRepository = bookRepository;
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<Author> GetAuthor()
+        {
+            var books = await _bookRepository.GetAllBooksAsync();
+            var authorWithMostExpensiveBook = books
+                .OrderByDescending(b => b.Price)
+                .Select(b => b.Author)
+                .FirstOrDefault();
+
+            return authorWithMostExpensiveBook;
+        }
+
+        public async Task<List<Author>> GetAuthors()
+        {
+            var authors = await _authorRepository.GetAllAuthorsAsync(); 
+            var books = await _bookRepository.GetAllBooksAsync();
+
+            var filteredAuthors = authors.Where(author =>
+                books.Count(book => book.AuthorId == author.Id && book.PublishDate.Year > 2015) % 2 == 0);
+
+            return filteredAuthors.ToList();
+        }
+    }
+}
+
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,37 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<Book> GetBook()
+        {
+            var books = await _bookRepository.GetAllBooksAsync(); 
+
+            return books
+                .OrderByDescending(b => b.Price)
+                .FirstOrDefault();
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            var carolusRexReleaseDate = new DateTime(2012, 5, 25);
+
+            var books = await _bookRepository.GetAllBooksAsync(); 
+
+            return books
+                .Where(b => b.Title.Contains("Red", StringComparison.OrdinalIgnoreCase)
+                             && b.PublishDate > carolusRexReleaseDate)
+                .ToList();
+        }
+    }
+}
+
